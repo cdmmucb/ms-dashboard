f7d36bf3887e24386d6c032875e36eb0974a0d2c
cdmmucb <carlos.mendizabal@ucb.edu.bo>
2022-06-04 18:29:11 -0400

    first commit

---
+++ src/test/java/com/example/msdashboard/MsDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
